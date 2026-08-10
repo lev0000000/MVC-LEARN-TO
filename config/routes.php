17547319977df1c2f5c927ce65196df5043f195a
lev0000000 <163733704+lev0000000@users.noreply.github.com>
2026-08-10 22:36:21 +0500

    32424DFE$

--- config/routes.php
+++ config/routes.php
@@ -11,3 +11,6 @@ $app->router->add('/', function(){
 
 $app->router->get('/test', [\App\Controllers\HomeController::class, 'test']);
 $app->router->post('/contact', [\App\Controllers\ContactController::class, 'test']);
+$app->router->get('/post/(?P<slug>[a-z0-9]+/?)', function(){
+    return 'Some Post';
+});
